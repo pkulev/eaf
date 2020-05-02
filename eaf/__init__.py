902dada653b02e4ae60d2d21fef77e37d0fbdfd2
Pavel Kulyov <kulyov.pavel@gmail.com>
2020-05-02 18:43:23 +0300

    Provide all useful things on the top level
    Signed-off-by: Pavel Kulyov <kulyov.pavel@gmail.com>

--- eaf/__init__.py
+++ eaf/__init__.py
@@ -5,3 +5,17 @@ enterprise-grade distributed and not applications.
 
 Also means Extensible As Fuck.
 """
+
+from eaf.app import Application
+from eaf.core import Vec3
+from eaf.render import Renderer, Renderable
+from eaf.state import State
+
+
+__all__ = [
+    Application.__name__,
+    Renderable.__name__,
+    Renderer.__name__,
+    State.__name__,
+    Vec3.__name__,
+]
